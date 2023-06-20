087237f204e9e868b22f31b85fcfa62eb9e4b770
Aritra Baidik <aritrabaidik@gmail.com>
2023-06-20 19:37:33 +0530

    commit

---
+++ HSU_OpenAI/app.js
@@ -0,0 +1,14 @@
+const { generateAnswer, generateImage } = require('./controllers/openaiController')
+const express = require('express')
+
+//app set up
+const app = express()
+app.listen(4000, () => console.log('listening for resquests on port 4000'))
+
+//middleware
+app.use(express.json())
+app.use(express.static('public'))
+
+//routes
+app.post('/openai/meta', generateAnswer)
+app.post('/openai/image', generateImage)
