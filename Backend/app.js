871fc2846e8453c0bb5651c4785b409cf9ea7580
Aritra Baidik <aritrabaidik@gmail.com>
2023-06-21 10:06:59 +0530

    c

--- Backend/app.js
+++ Backend/app.js
@@ -1,9 +1,14 @@
 const { generateAnswer, generateImage } = require('./controllers/openaiController')
 const express = require('express')
+const cors = require('cors')
+
 
 //app set up
 const app = express()
 app.listen(4000, () => console.log('listening for resquests on port 4000'))
+
+//cors setup
+app.use(cors({}))
 
 //middleware
 app.use(express.json())
