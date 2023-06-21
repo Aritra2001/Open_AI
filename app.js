fa15fd11620bf9ede91d01e384c2de1422a31f5a
Aritra Baidik <aritrabaidik@gmail.com>
2023-06-21 09:10:50 +0530

    commit

--- app.js
+++ app.js
@@ -10,5 +10,5 @@ app.use(express.json())
 app.use(express.static('public'))
 
 //routes
-app.post('/', generateAnswer)
-app.post('/', generateImage)
+app.post('', generateAnswer)
+app.post('', generateImage)
