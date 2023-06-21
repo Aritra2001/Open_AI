1b854a699b2ea06f0377c0016ebc94d43d151854
Aritra Baidik <aritrabaidik@gmail.com>
2023-06-21 09:55:18 +0530

    commit

--- Backend/app.js
+++ Backend/app.js
@@ -7,7 +7,7 @@ app.listen(4000, () => console.log('listening for resquests on port 4000'))
 
 //middleware
 app.use(express.json())
-app.use(express.static('public'))
+app.use(express.static('../public'))
 
 //routes
 app.post('/openai/meta', generateAnswer)
